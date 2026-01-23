f7d6e81fa293d8ca955d9561c8fd376e569a9ce0
Bharat Guri <mr.bharat125560@gmail.com>
2026-01-23 13:30:30 +0530

    update 23-1-26

--- room-project/src/pages/SearchGym.jsx
+++ room-project/src/pages/SearchGym.jsx
@@ -1,19 +1,21 @@
 import DashboardLayout from "../components/DashboardLayout";
 import { useNavigate } from "react-router-dom";
-
+import DefaultImg from "../assets/gym/empty-gym.webp"
+import DefaultImg2 from "../assets/gym/2.webp"
+import DefaultImg3 from "../assets/gym/3.webp"
 
 export default function SearchGym() {
   const gyms = [
-    { id: 1, name: "Gold Gym", place: "Mansarovar", rating: "4.5" },
-    { id: 2, name: "Fitness Arena", place: "Vaishali Nagar", rating: "4.2" },
-    { id: 3, name: "Power House Gym", place: "Malviya Nagar", rating: "4.7" },
+    { id: 1, name: "Gold Gym", place: "Hisar", rating: "4.5" ,imageUrl:DefaultImg, },
+    { id: 2, name: "Fitness Arena", place: "Red Square (Hisar)", rating: "4.2" ,imageUrl:DefaultImg2, },
+    { id: 3, name: "Power House Gym", place: "Rishi Nagar (Hisar)", rating: "4.7" ,imageUrl:DefaultImg3, },
   ];
 
   const navigate = useNavigate();
 
   return (
     <DashboardLayout>
-      <div className="flex items-center justify-between mb-6">
+      <div className="flex items-center justify-between mb-6">  
         <h2 className="text-2xl font-bold">Search Gym</h2>
         <input
           placeholder="Search gym name / area..."
@@ -24,12 +26,17 @@ export default function SearchGym() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {gyms.map((g) => (
           <div key={g.id} className="bg-white p-5 rounded-xl shadow hover:shadow-lg transition">
-            <div className="h-32 bg-gray-200 rounded-lg mb-4"></div>
-            <h3 className="text-lg font-semibold">{g.name}</h3>
-            <p className="text-gray-500 text-sm">{g.place}</p>
-            <p className="mt-2 text-sm font-semibold text-green-600">
-              ⭐ Rating: {g.rating}
-            </p>
+
+            <div className="">
+              <img src={g.imageUrl || DefaultImg} />
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">{g.name}</h3>
+              <p className="text-gray-500 text-sm">{g.place}</p>
+              <p className="mt-2 text-sm font-semibold text-green-600">
+                ⭐ Rating: {g.rating}
+              </p>
+            </div>
 
             <button
               onClick={() => navigate(`/gym/${g.id}`)}
--- room-project/src/pages/Supplements.jsx
+++ room-project/src/pages/Supplements.jsx
@@ -1,17 +1,21 @@
 import DashboardLayout from "../components/DashboardLayout";
-// import DefaultImg from "../assets/trainers/2.webp"
+import DefaultImg from "../assets/supliment/weyprotin.webp"
 import DefaultImg2 from "../assets/supliment/Cratin.webp"
-
+import DefaultImg3 from "../assets/supliment/multi.webp"
+import DefaultImg4 from "../assets/supliment/Fish.webp"
+import DefaultImg5 from "../assets/supliment/protin.webp"
+import DefaultImg6 from "../assets/supliment/gainner.webp"
+import DefaultImg7 from "../assets/supliment/PreWorkout.webp"
 
 export default function Supplements() {
   const products = [
     { id: 1, name: "Whey Protein", price: "₹1999",imageUrl:DefaultImg, },
     { id: 2, name: "Creatine", price: "₹899",imageUrl:DefaultImg2, },
-    { id: 3, name: "Multivitamin", price: "₹499" },
-     { id: 4, name: "Fish oil", price: "₹2199" },
-      { id: 5, name: "Protin", price: "₹2499" },
-       { id: 6, name: "Wight Gainner", price: "₹1499" },
-        { id: 7, name: "Pre Workout", price: "₹699" },
+    { id: 3, name: "Multivitamin", price: "₹499" ,imageUrl:DefaultImg3,},
+     { id: 4, name: "Fish oil", price: "₹2199" ,imageUrl:DefaultImg4,},
+      { id: 5, name: "Protin", price: "₹2499",imageUrl:DefaultImg5, },
+       { id: 6, name: "Wight Gainner", price: "₹1499",imageUrl:DefaultImg6, },
+        { id: 7, name: "Pre Workout", price: "₹699" ,imageUrl:DefaultImg7,},
   ];
 
   return (
@@ -22,7 +26,7 @@ export default function Supplements() {
         {products.map((p) => (
           <div key={p.id} className="bg-white p-5 rounded-xl shadow hover:shadow-lg transition">
             <div className="">
-              <img src={t.imageUrl || DefaultImg}/>
+              <img src={p.imageUrl || DefaultImg}/>
             </div>
             <div>
             <h3 className="text-lg font-semibold">{p.name}</h3>
